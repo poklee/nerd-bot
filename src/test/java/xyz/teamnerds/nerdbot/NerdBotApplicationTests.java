1fefc7b75a3477f834f96c4e5ec287ac3183e2b3
Pok Lee <pok.lee@gmail.com>
2020-07-01 22:20:52 -0400

    Remove spring load test

--- src/test/java/xyz/teamnerds/nerdbot/NerdBotApplicationTests.java
+++ src/test/java/xyz/teamnerds/nerdbot/NerdBotApplicationTests.java
@@ -1,13 +1,6 @@
 package xyz.teamnerds.nerdbot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NerdBotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class NerdBotApplicationTests
+{
 
 }
